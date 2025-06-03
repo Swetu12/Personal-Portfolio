b2922ac6b825c804f7c18a8dba5bdc12282fcf13
Swetu12 <glowminty12@gmail.com>
2025-06-03 20:59:21 +0100

    >

--- src/constants/Testimonials.ts
+++ src/constants/Testimonials.ts
@@ -8,7 +8,7 @@ export const testimonials = {
       job: "CEO of rolleenagency",
       review:
         "I recently worked with Alex on building a website for my photography and videography business, and I couldn’t be happier with the experience. From the very start, Alex was incredibly communicative, keeping me updated throughout the entire process. He took the time to explain everything in a way that was easy to understand, making sure I felt confident in using all the features of the website. Not only did he create a sleek, professional site that perfectly represents my brand, but he also made the whole process seamless and stress-free. His attention to detail and willingness to tailor everything to my needs really stood out. If you’re looking for a website builder who is skilled, reliable, and a great communicator, I highly recommend Alex for any future projects! ",
-      image: "/hero/halexx.webp",
+      image: "/userplaceholder.svg",
     },
   ],
 };
